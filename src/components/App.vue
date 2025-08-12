46bb674bfe1b29cdfa46fb6013e659e51041b8e4
Sofia Noor Rafa <sofiautilitarian@email.com>
2025-08-12 10:41:07 +0600

    Happy coding! 🧠💻

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <main>
+  
+    <!--heading!-->
+    
+    <header><img src='../assets/pinia-logo.svg' alt='logo'></img>
+      <h1>Pinia Tasks </h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
